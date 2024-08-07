364c71bba1a77f7cb2508bff7a878f2009e0aabb
Daniel Choi <wydanielchoi@gmail.com>
2024-08-07 21:26:38 +0900

    fixed main.py

--- datareader/main.py
+++ datareader/main.py
@@ -3,4 +3,4 @@ from src.datareader import KlineDataDownloader
 
 if __name__ == '__main__':
     parser = KlineDataDownloader()
-    parser.download_data('CRYPTO_SPOT', ['BTCUSDT', 'ETHUSDT'], '1m', 202407200000, 202407270000)
+    parser.download_data('CRYPTO_SPOT_BINANCE', ['BTCUSDT', 'ETHUSDT'], '1m', 202407200000, 202407270000)
